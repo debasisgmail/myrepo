9734dca8d8a6d9efd3de698fd61144623719dd39
debasisgmail <debasissingh.72@gmail.com>
2021-10-01 12:49:42 +0530

    added folder
    added folder

--- print_random_numbers_1-10.py
+++
@@ -1,7 +0,0 @@
-
-#imporitng a module used for generating ramndom numbers
-import random
-#variable declaration which stores the random generated numbers with a certain range.
-my_randoms = random.sample(range(0, 10), 10)
-#printing the output
-print(my_randoms)
